bd6b6b2d855a6707e9ab27facb121ae5ba5f8162
Alexsandr Kardash <areswarmoney01@gmail.com>
2025-11-06 11:48:48 +0200

    init js

--- js/practic-1.js
+++ js/practic-1.js
@@ -1,1 +1,10 @@
+
+window.addEventListener('load', () => {
+  if (typeof window.ethereum !== 'undefined') {
+    console.log('MetaMask detected');
+  } else {
+    console.log('No ethereum provider found');
+    // Можеш інжектити свій тут
+  }
+});
 console.log(Number("5")); // 5");
